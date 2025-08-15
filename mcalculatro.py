025bca4f3147c1588f97a7b583168596be629070
Jose Castro <ustr005624.ugb.edu.sv>
2025-08-15 10:14:58 -0600

    se hizo la minicalculadora

---
+++ mcalculatro.py
@@ -0,0 +1,9 @@
+a = float (input ("Ingrese el primer numero: "))
+b = float (input ("Ingrese el segundo numero: "))
+
+
+print("suma:", a+b)
+print("Resta: ", a-b)
+print("Multiplicacion", a*b)
+print ("division: ", a/b)
+
