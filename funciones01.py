c0da3f930ddc77eeb827ea7dc5f61aa9228d370b
Jose Castro <ustr005624.ugb.edu.sv>
2025-08-22 09:47:25 -0600

    cuadrado de un numero

---
+++ funciones01.py
@@ -0,0 +1,8 @@
+def cuadrado_numero(num1):
+    return num1 **2
+
+#pedir la variable
+num1=int(input("Introduce el numero para calcular:  "))
+
+#imprimir el resultado
+print(f"El cuadrado de {num1} es {cuadrado_numero(num1)}")
