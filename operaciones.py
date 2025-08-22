8f10cb8c000e07b05193c6f9a1f04f8220e18a45
Jose Castro <ustr005624.ugb.edu.sv>
2025-08-22 08:59:36 -0600

    se hizo la minicalculadora, lo de la bienvenida con el nombre y el numero mayor

---
+++ operaciones.py
@@ -0,0 +1,18 @@
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+
+suma = num1+num2
+resta = num1-num2
+Multiplicacion = num1*num2
+
+
+if num2 != 0:
+
+    division = "No se puede dividir entre 0"
+
+    print(suma ,":suma")
+    print(resta ,":resta")
+    print(Multiplicacion ,":Multiplicacion")
+    print(division ,":division ")
+
+
