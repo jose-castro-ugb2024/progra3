d6a8c6f0c5d5c03091bba696194e497ff5df1432
Jose Castro <ustr005624.ugb.edu.sv>
2025-08-29 08:50:30 -0600

    se hizo el switchs

---
+++ switch01.py
@@ -0,0 +1,12 @@
+def operaciones01(operador, num1, num2):
+    if operador=="suma":
+        return num1+num2
+    elif operador=="resta":
+        return num1-num2
+    elif operador=="multi":
+        return num1*num2
+    elif operador== "divi":
+        return num1/num2
+
+    operaciones01(operador,"suma", num1:8, num2:2)
+    print(operaciones01)
