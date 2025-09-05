085e6a2254c8831ee97ef01175b8eca7faf5b0dc
Jose Castro <ustr005624.ugb.edu.sv>
2025-09-05 09:09:50 -0600

    se hizo progranma que calcula cuantos años tienes

---
+++ Promedio_computo01.py
@@ -0,0 +1,13 @@
+def calcular_promedio(lab1, lab2, parcial):
+
+    nota_final = (lab1 * 0.30) + (lab2 * 0.30) + (parcial * 0.40)
+    return nota_final
+
+
+print("=== Calculadora de Promedio ===")
+lab1 = float(input("Ingresa la nota del Laboratorio 1 (0 - 10): "))
+lab2 = float(input("Ingresa la nota del Laboratorio 2 (0 - 10): "))
+parcial = float(input("Ingresa la nota del Parcial (0 - 10): "))
+
+promedio = calcular_promedio(lab1, lab2, parcial)
+print(f"Tu nota
