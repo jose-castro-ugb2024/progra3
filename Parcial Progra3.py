7b5539973f814941ca57bde6b21fbb43f7b00375
Jose Castro <ustr005624.ugb.edu.sv>
2025-09-05 09:22:50 -0600

    se hizo programa que calcula tu promedio en el computo 1

--- Parcial Progra3.py
+++ Parcial Progra3.py
@@ -4,15 +4,15 @@ from datetime import date
 
 def calcular_edad(dia, mes, año):
     hoy = date.today()
-    edad = hoy.year - año
+    edad = hoy.year-año
 
-    # Ajustar si aún no ha pasado tu cumpleaños
+    # Arreglar si aún no ha pasado tu cumpleaños
     if (hoy.month, hoy.day) < (mes, dia):
         edad -= 1
 
     return edad
 
-
+#el calculo de la edad con tu dia, mes y año
 print("=== Calculadora de Edad ===")
 dia = int(input("Ingresa tu día de nacimiento: "))
 mes = int(input("Ingresa tu mes de nacimiento (número): "))
